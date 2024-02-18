38529fdab426d5ddfd6ffd497a65a3596bde9a55
DaPiankov <DaPiankov@croc.ru>
2024-02-18 15:23:06 +0300

    [feat]: Добавил базовую структуру

---
+++ src/test/java/heavenboards/project/service/ProjectServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package heavenboards.project.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * Тест поднятия контекста.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+class ProjectServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
