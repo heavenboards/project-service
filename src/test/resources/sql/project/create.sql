12c40a0a30132383d93aad6dd95ed55698d7fafb
Pyankov Daniil <109591434+pyankovdaniil@users.noreply.github.com>
2024-02-19 17:46:55 +0300

    [feat]: Добавил тесты (#3)
    Co-authored-by: DaPiankov <DaPiankov@croc.ru>

---
+++ src/test/resources/sql/project/create.sql
@@ -0,0 +1,9 @@
+INSERT INTO project_entity(id, name)
+VALUES ('f91b820e-7721-43ad-b188-1cbd6c0b9d42', 'godsystem'),
+       ('dba22a9b-afe1-4002-8158-98d7d6585dfd', 'orpheus');
+
+INSERT INTO project_user_entity(id, user_id, project_id)
+VALUES ('1933084d-5a6a-4437-a9a4-ccd3ece0bd15', '4b15db5a-d772-4826-aec7-2772fb036e1d',
+        'f91b820e-7721-43ad-b188-1cbd6c0b9d42'),
+       ('3c941789-3197-4928-a182-344116f24bdc', '4b15db5a-d772-4826-aec7-2772fb036e1d',
+        'dba22a9b-afe1-4002-8158-98d7d6585dfd');
