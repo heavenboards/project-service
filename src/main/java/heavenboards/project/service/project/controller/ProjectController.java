b22fcfa6d2e5411924281ed426b7bd242b988de3
DaPiankov <DaPiankov@croc.ru>
2024-03-06 19:33:13 +0300

    [feat]: Добавил @CrossOrigin, поправил названия

--- src/main/java/heavenboards/project/service/project/controller/ProjectController.java
+++ src/main/java/heavenboards/project/service/project/controller/ProjectController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import java.util.UUID;
  * Контроллер для взаимодействия с проектами.
  */
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/project")
 @Tag(name = "ProjectController", description = "Контроллер для взаимодействия с проектами")
@@ -62,8 +64,8 @@ public class ProjectController {
      */
     @GetMapping
     @Operation(summary = "Получить все проекты пользователя")
-    public List<ProjectTo> getAllProjects() {
-        return projectFindUseCase.getAllProjects();
+    public List<ProjectTo> findUserProjects() {
+        return projectFindUseCase.findUserProjects();
     }
 
     /**
